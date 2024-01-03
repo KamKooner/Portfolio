971a857e0bac2634886f4cb92181ae31a927514e
KamKooner <k1kooner@ryerson.ca>
2024-01-03 16:04:38 -0500

    edits

---
+++ src/app/components/title.tsx
@@ -0,0 +1,11 @@
+import Image from 'next/image'
+
+export default function title() {
+  return (
+    <div className="ml-8">
+      <h1 className="text-white">Kam</h1>
+      <h2>Experienced Software Engineer</h2>
+      <h5>I build pixel-perfect, accessible products for the web and beyond.</h5>
+    </div>
+  )
+}
