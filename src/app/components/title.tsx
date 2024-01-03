ae516d19069cdcd2191d25efe389ac9f4998fb7f
KamKooner <k1kooner@ryerson.ca>
2024-01-03 18:22:25 -0500

    Title Card

--- src/app/components/title.tsx
+++ src/app/components/title.tsx
@@ -2,10 +2,11 @@ import Image from 'next/image'
 
 export default function title() {
   return (
-    <div className="ml-8">
-      <h1 className="text-white">Kam</h1>
-      <h2>Experienced Software Engineer</h2>
-      <h5>I build pixel-perfect, accessible products for the web and beyond.</h5>
+    <div className="ml-8 mt-8">
+      <p className="t1">Kam Kooner</p>
+      <p className="t2">Experienced Software Engineer</p>
+      <p className="t3 mb-0">I build pixel-perfect, accessible products </p>
+      <p className='t3'>for the web and beyond.</p>
     </div>
   )
 }
