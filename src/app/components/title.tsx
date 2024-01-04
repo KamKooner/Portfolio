fa04b579bc0a4bb189468677f7f2a0939aa6c65b
KamKooner <k1kooner@ryerson.ca>
2024-01-04 17:58:20 -0500

    title and about

--- src/app/components/title.tsx
+++ src/app/components/title.tsx
@@ -2,10 +2,10 @@ import Image from 'next/image'
 
 export default function title() {
   return (
-    <div className="ml-8 mt-8">
+    <div className="title flex flex-col w-1/3 items-start">
       <p className="t1">Kam Kooner</p>
-      <p className="t2">Experienced Software Engineer</p>
-      <p className="t3 mb-0">I build pixel-perfect, accessible products </p>
+      <p className="t2 ">Experienced Software Engineer</p>
+      <p className="t3 mt-4">I build pixel-perfect, accessible products </p>
       <p className='t3'>for the web and beyond.</p>
     </div>
   )
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,17 @@
-import Image from 'next/image'
-import Title from './components/title'
+// pages/index.js
+
+import React, { useEffect } from 'react';
+import Title from './components/title';
+import About from './components/about';
 export default function Home() {
+
   return (
     <body>
-      
-      <Title />
+      <div className='flex mt-[100px] h-screen justify-start ml-[360px] '>
+        <Title />
+        <About />
+      </div>
       
     </body>
-  )
+  );
 }
