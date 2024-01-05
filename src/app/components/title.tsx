f7e508e4a31e363eb4edde09a72cc22f5456221f
KamKooner <k1kooner@ryerson.ca>
2024-01-05 16:41:48 -0500

    sections created, socials added, stylized

--- src/app/components/title.tsx
+++ src/app/components/title.tsx
@@ -3,10 +3,38 @@ import Image from 'next/image'
 export default function title() {
   return (
     <div className="title flex flex-col w-1/3 items-start">
-      <p className="t1">Kam Kooner</p>
-      <p className="t2 ">Experienced Software Engineer</p>
-      <p className="t3 mt-4">I build pixel-perfect, accessible products </p>
-      <p className='t3'>for the web and beyond.</p>
+      <div>
+        <p className="t1">Kam Kooner</p>
+        <p className="t2 ">Experienced Software Engineer</p>
+        <p className="t3 mt-4">I build pixel-perfect, accessible products </p>
+        <p className='t3'>for the web and beyond.</p>
+      </div>
+
+
+
+
+      <div className="mt-16 selectors">
+        <div className="t3 flex" id="about">
+          <hr className='mt-2'></hr><p className="mb-9 ml-2">ABOUT</p>
+        </div>
+      
+        <div className="t3 flex" id="experience">
+          <hr className='mt-2'></hr><p className="mb-9 ml-2">EXPERIENCE</p>
+        </div>
+
+        <div className="t3 flex" id="projects">
+          <hr className='mt-2'></hr><p className="mb-9 ml-2">PROJECTS</p>
+        </div>
+      </div>
+
+
+      <div className="social-icons mt-[350px]">
+        <a href="https://github.com/KamKooner" target="_blank" className="social-icon"><i className="fab fa-github"></i></a>
+        <a href="https://www.linkedin.com/in/kam-kooner-941371174/" target="_blank" className="social-icon"><i className="fab fa-linkedin"></i></a>
+        <a href="https://www.google.com" target="_blank" className="social-icon"><i className="fab fa-instagram"></i></a>
+      </div>
+
+      
     </div>
   )
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,11 +7,12 @@ export default function Home() {
 
   return (
     <body>
+      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css"></link>
       <div className='flex mt-[100px] h-screen justify-start ml-[360px] '>
         <Title />
+        <div className="flex flex-col w-1/3 mr-8"></div>
         <About />
       </div>
-      
     </body>
   );
 }
