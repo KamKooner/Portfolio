7c6d57b7e7215293d1348f793d0393ec24e80b36
KamKooner <k1kooner@ryerson.ca>
2024-01-06 18:30:04 -0500

    t2

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    experimental: {
+        images: true, // Enable the images experimental feature
+      },
+}
 
 module.exports = nextConfig
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,22 @@
 // pages/index.js
-import { useState } from 'react';
-
 import React, { useEffect } from 'react';
 import Title from './components/title';
 import About from './components/about';
+import Flashlight from './components/flashlight';
 
 export default function Home() {
+  
 
   return (
     <body>
+      <Flashlight />
       <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css"></link>
       <div className='flex mt-[100px] h-screen justify-start ml-[360px] '>
         <Title />
         <div className="flex flex-col w-1/3 mr-8"></div>
         <About />
       </div>
-
+      
     </body>
   );
 }
