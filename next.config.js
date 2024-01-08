d92de4e5ba3b654248f0885c47f4fa78b0226963
KamKooner <k1kooner@ryerson.ca>
2024-01-08 12:29:49 -0500

    conf1

--- next.config.js
+++ next.config.js
@@ -1,8 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-    experimental: {
-        images: true, // Enable the images experimental feature
-      },
-}
+const nextConfig = {}
 
 module.exports = nextConfig
