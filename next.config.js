45673bfe58db1d57c82938ec75f8c2e61126e9ff
Kam Kooner <85206896+KamKooner@users.noreply.github.com>
2024-01-08 14:15:48 -0500

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -2,9 +2,8 @@
 const nextConfig = {
   // Other configuration options...
 
-  // Add the following lines to enable static HTML export
-  // See: https://nextjs.org/docs/advanced-features/static-html-export
-  output: 'out/',
+  // Remove the following line, as we're handling export in GitHub Actions
+  // output: 'out/',
 };
 
 module.exports = nextConfig;
