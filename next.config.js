9ab72faa36f93138c16b53795129396071de9048
Kam Kooner <85206896+KamKooner@users.noreply.github.com>
2024-01-08 14:58:45 -0500

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -5,7 +5,7 @@ const nextConfig = {
   // Remove the following line, as we're handling export in GitHub Actions
   // output: 'out/',
   // Add the following line for static HTML export
-  output: 'standalone' | 'export',
+  output: 'standalone',
 };
 
 module.exports = nextConfig;
