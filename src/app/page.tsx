5ed0e64658789ba17943730d530cc81fe29c4544
KamKooner <k1kooner@ryerson.ca>
2024-01-08 20:28:33 -0500

    changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,12 @@
+'use client'
 // pages/index.js
 import React, { useEffect } from 'react';
 import Title from './components/Title';
-
-import Flashlight from './components/flashlight';
 import About from './components/About';
 export default function Home() {
-  
 
   return (
     <body>
-      <Flashlight />
       <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css"></link>
       <div className='flex mt-[100px] h-screen justify-start ml-[360px] '>
         <Title />
